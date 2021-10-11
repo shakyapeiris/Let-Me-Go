94d13b8d2e775b9e2e16489b37d6b9f6cdd5450e
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-11 20:13:37 +0530

    change live update from async  to websocket and finish the user Home

--- mobile/lib/screens/userHome.dart
+++ mobile/lib/screens/userHome.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/components/user_home/topCard.dart';
 import 'package:mobile/components/user_home/map.dart';
-// import 'package:http/http.dart' as http;
+import 'package:socket_io_client/socket_io_client.dart' as Io;
+import '../utils/userHomeUtils.dart';
 // import 'package:geolocator/geolocator.dart';
-// import 'dart:async';
 
-// 1. Find users location
-// 2. Send it to the backend with a time out
-// 3. act according to the response
 class UserHome extends StatefulWidget {
   const UserHome({Key? key}) : super(key: key);
 
@@ -18,37 +15,38 @@ class UserHome extends StatefulWidget {
 class _UserHomeState extends State<UserHome> {
   bool stopped = true;
   bool isAmbulance = false;
+  late Object? ambulanceData;
+  Io.Socket socket = Io.io(URL);
 
-  String getMainTopic() => stopped
-      ? "Stopped"
-      : isAmbulance
-          ? "An ambulance"
-          : "Go Forward";
-  String getSubTopic() => stopped
-      ? "Click on the red button to continue"
-      : isAmbulance
-          ? "Seems like an ambulance near by you"
-          : "No problems continue your journey";
+  late HomeUtils homeUtils;
+
   @override
+  void initState() {
+    super.initState();
+    homeUtils = new HomeUtils(
+        stopped: stopped,
+        isAmbulance: isAmbulance,
+        ambulanceData: ambulanceData,
+        socket: socket,
+        setState: setState);
+    homeUtils.setSocket();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           TopCard(
-            mainTopic: getMainTopic(),
-            subTopic: getSubTopic(),
+            mainTopic: homeUtils.getMainTopic(),
+            subTopic: homeUtils.getSubTopic(),
           ),
           DetailsMap(),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: stopped ? Colors.red[700] : Colors.green[700],
-        child: stopped ? Icon(Icons.play_arrow_rounded) : Icon(Icons.pause),
-        onPressed: () {
-          setState(() {
-            stopped = !stopped;
-          });
-        },
+        backgroundColor: homeUtils.getBackGroundColor(),
+        child: homeUtils.getIcon(),
+        onPressed: homeUtils.toggleStatus(),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
