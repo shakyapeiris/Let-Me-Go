dafcbc3d179aa107dd65bb72bee5f017fe71cd7b
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-12 19:34:16 +0530

    set a background color and fix width.height yet to make responsive

--- mobile/lib/components/ui/button.dart
+++ mobile/lib/components/ui/button.dart
@@ -5,24 +5,31 @@ class Button extends StatelessWidget {
   final EdgeInsetsGeometry padding;
   final EdgeInsetsGeometry margin;
   final Widget child;
+  final Color? backgroundColor;
   const Button({
     Key? key,
     required this.onPressed,
     required this.padding,
     required this.margin,
     required this.child,
+    required this.backgroundColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: onPressed,
-        child: Container(
-          padding: padding,
-          margin: margin,
-          child: child,
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        alignment: Alignment.center,
+        height: 50.0,
+        width: 300.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50.0),
+          color: backgroundColor,
         ),
+        padding: padding,
+        margin: margin,
+        child: child,
       ),
     );
   }
